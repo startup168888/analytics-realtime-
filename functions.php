42797db9bb87e3fd57bc228550cf51a837d36528
Jeff-Loh <90755610+Jeff-Loh@users.noreply.github.com>
2021-11-06 02:03:27 +0800

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-define('VIEW','G-0MTZQJXXZQ');
+define('PROPERTY','289140302');
 define('SERVICE_ACCOUNT', 'e-commerce-331216-b3aa31150daa.json');
 define('DOMAIN','https://test-bot011.herokuapp.com/');
 function getActivePages($analytics){
